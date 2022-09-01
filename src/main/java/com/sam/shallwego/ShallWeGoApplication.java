54115d722168c1cece601907bea29e7a963689c7
iqpizza6349 <iqpizza6349@gmail.com>
2022-09-01 09:30:53 +0900

    :tada: :: project start

---
+++ src/main/java/com/sam/shallwego/ShallWeGoApplication.java
@@ -0,0 +1,13 @@
+package com.sam.shallwego;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShallWeGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShallWeGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sam/shallwego/ShallWeGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sam.shallwego;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShallWeGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
