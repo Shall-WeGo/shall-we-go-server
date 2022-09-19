9d4e9c19836903118784df91a75846c0dc45c2e7
iqpizza6349 <iqpizza6349@gmail.com>
2022-09-19 13:22:58 +0900

    :recycle: :: (#23) refactor member service

---
+++ src/main/java/com/sam/shallwego/global/config/MyConfiguration.java
@@ -0,0 +1,30 @@
+package com.sam.shallwego.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+@EnableWebFlux
+@ComponentScan
+@RequiredArgsConstructor
+public class MyConfiguration implements WebFluxConfigurer {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        configurer.defaultCodecs().jackson2JsonEncoder(
+            new Jackson2JsonEncoder(objectMapper)
+        );
+        configurer.defaultCodecs().jackson2JsonDecoder(
+            new Jackson2JsonDecoder(objectMapper)
+        );
+    }
+}
